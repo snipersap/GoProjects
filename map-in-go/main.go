22f75f3792a9a5fd9ee859a552f61e0f8c9511cf
snipersap <snipersap@gmail.com>
2023-09-13 20:45:29 +0200

    var map init, add and  delete

--- map-in-go/main.go
+++ map-in-go/main.go
@@ -1,5 +1,43 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// Map storing the hex codes of colors
+	colorCodes := map[string]string{
+		"red":   "#ff0000",
+		"green": "#008000",
+		"blue":  "#0000ff",
+	}
+	fmt.Println(colorCodes)
+	fmt.Printf("Color codes formatted:%s\n", colorCodes)
+
+	// Map of bus routes
+	var busRoutes map[int]string
+	busRoutes = map[int]string{94: "Hochschule"}
+	busRoutes[97] = "Londoner Ring"
+	busRoutes[77] = "LU Rathaus"
+	printI(busRoutes)
+
+	//Map of train routes
+	trainRoutes := make(map[string]string, 4)
+	trainRoutes = map[string]string{
+		"ICE5":    "Frankfurt to Basel",
+		"ICE5685": "Frankfurt to Paris Est.",
+		"EC52":    "Mannheim to Luzern",
+		"IC90":    "Freiburg to Berlin",
+	}
+	printS(trainRoutes)
+	delete(trainRoutes, "ICE5685") //Delete a key
+	printS(trainRoutes)
+
+}
+
+func printI(ism map[int]string) {
+	fmt.Println(ism)
+}
+
+func printS(ssm map[string]string) {
+	fmt.Println(ssm)
 }
---
+++ map-in-go/readme.md
@@ -0,0 +1,26 @@
+# ReadMe
+This repo is the project *map-in-go* as described in the course  [Go: The Complete Developer's Guide (Golang)](https://udemy.com/course/go-the-complete-developers-guide/). 
+
+  
+
+## How to test the repo?
+Run the files `main.go` using the command 
+`go run main.go`.   
+
+Run the tests with 
+`go test`
+
+### Test Result:
+  
+
+In case of confusion, check the commits. 
+
+## Expected output
+Last updated 13.09.2023  
+>  
+
+## Report bugs or suggestions
+Please use the *Issues* feature in github to raise one. There is no guarantee or promise to fix it, because, well, this repo is a project after all. However, all suggestions are welcome. 
+
+## Disclaimer
+Please use the code here at your own risk. This is a sample project, and may not always provide the expected outcome or result. 
