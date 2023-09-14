8376d4908ec98224af491e82ca1e5b1b9a270bda
snipersap <snipersap@gmail.com>
2023-09-14 19:27:22 +0200

    implemented shape interface

---
+++ shape-area-w-interface/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	area() float64
+	name() string
+}
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+func main() {
+
+	s := square{109}
+	t := triangle{height: 89, base: 96}
+	print(s)
+	print(t)
+}
+
+// Returns area of a triangle
+func (t triangle) area() float64 {
+	return t.base * t.height / 2
+}
+
+// Returns area of a square
+func (s square) area() float64 {
+	return s.sideLength * s.sideLength
+}
+
+// Return the name of the shape
+func (t triangle) name() string {
+	return "Triangle"
+}
+
+func (s square) name() string {
+	return "Square"
+}
+
+// Print the area of an object
+func print(sh shape) {
+	a := sh.area()
+	n := sh.name()
+	fmt.Println("Area of", n, "is", a)
+
+}
