44f871b597c8606e5238f5921d55267200ef106d
snipersap <snipersap@gmail.com>
2023-09-25 18:04:01 +0200

    used go convention for error msgs: no start caps

--- web-server-example/main.go
+++ web-server-example/main.go
@@ -16,7 +16,7 @@ func main() {
 	log.Println("Starting web server on port:", portNumber)
 	wErr := http.ListenAndServe(":8080", nil)
 	if wErr != nil {
-		log.Fatalln("Couldn't start the web server on port 8080. Error was:", wErr.Error())
+		log.Fatalln("couldn't start the web server on port 8080. Error was:", wErr.Error())
 	}
 
 }
